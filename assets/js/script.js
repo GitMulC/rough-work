72b01c6af36b7738dcf67d1d07b9c91fe4ed60b4
GitMulC <cmulvo@gmail.com>
2021-11-24 23:39:16 +0000

    Add more js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+var swing1 = document.getElementsByClassName('left-swing');
+var swing2 = document.getElementsByClassName('right-swing');
+var swing = [swing1, swing2];
+
+// swing1.addEventListener("click", getSwing);
+// swing2.addEventListener("click", getSwing);
+function getSwing () {
+    return swing[Math.floor(Math.random()*swing.length)];
+    // if (getSwing === ){
+
+    // }
+}
+console.log(getSwing());
+
+
 
 /**
  * Plays sound when clicking divs
