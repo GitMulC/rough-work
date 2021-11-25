da6dc9798e6c872ae94ce3508aa54d48b8c0276c
GitMulC <cmulvo@gmail.com>
2021-11-25 20:44:03 +0000

    Troubleshoot js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,30 @@
-var swing1 = document.getElementsByClassName('left-swing');
-var swing2 = document.getElementsByClassName('right-swing');
-var swing = [swing1, swing2];
+let swing1 = document.getElementById('left-swing');
+let swing2 = document.getElementById('right-swing');
+let swing = [swing1, swing2];
 
-// swing1.addEventListener("click", getSwing);
-// swing2.addEventListener("click", getSwing);
-function getSwing () {
+function getSwing (event) {
     return swing[Math.floor(Math.random()*swing.length)];
-    // if (getSwing === ){
-
-    // }
 }
 console.log(getSwing());
 
+/**
+ * Not working, don't know why. Want a listener to trigger when either div(swing1 & swing2) is clicked to run getSwing();
+ * then check if the random one generated is the same as the one clicked and if it is counts as a block.
+ */
+// swing1.addEventListener('click', getSwing()); 
+// swing2.addEventListener('click', getSwing());
+// document.querySelectorAll('div'); ???? How will this help?
+
+/**
+ * Not sure if this is done correctly?
+ * What do I put in the item place?
+ * How do I see this on the console to tell if it's working?
+ */
+document.querySelectorAll('.attack').forEach(item => {
+    item.addEventListener('click', event => {
+        return swing[Math.floor(Math.random()*swing.length)];
+    })
+})
 
 
 /**
