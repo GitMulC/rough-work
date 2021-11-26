74d19e04598de2a8c716ec002de8146075c34e28
GitMulC <cmulvo@gmail.com>
2021-11-26 00:15:15 +0000

    Format & add in more comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,22 @@
+/**
+ * Core game mechanic
+ * Uses boolean to check if attack lands.
+ * If it hits, plays gore sound & gladiator loses hitpoint.
+ * If blocks, plays block sound and no hit points lost.
+ */
 function getSwing () {
     return Math.floor(Math.random()*2);
 }
-console.log(getSwing());
 
-/**
- * Not working, don't know why. Want a listener to trigger when either div(swing1 & swing2) is clicked to run getSwing();
- * then check if the random one generated is the same as the one clicked and if it is counts as a block, and block sound will play.
- * Then if it isn't a block it's a successful hit on the gladiator who will lose one hitpoint.
- */
-// swing1.addEventListener('click', getSwing()); 
-// swing2.addEventListener('click', getSwing());
-// document.querySelectorAll('div'); ???? How will this help?
-
-/**
- * Not sure if this is done correctly?
- * What do I put in the item place?
- * How do I see this on the console to tell if it's working?
- */
-
+function swing (swingSide) {
+    let result = getSwing();
+    let blockedAttack = result === swingSide;
+    play(blockedAttack);
+}
 
 
 /**
- * Sounds
+ * Sounds played
  */
 function play(blockedAttack) {
     var audio = document.getElementById("audio");
@@ -32,10 +27,4 @@ function play(blockedAttack) {
     } else {
         audio2.play();
     }
-}
-
-function swing (swingSide) {
-    let result = getSwing();
-    let blockedAttack = result === swingSide;
-    play(blockedAttack);
 }
